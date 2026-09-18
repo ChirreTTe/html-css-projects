d9601c4574a859e925b09093dffcec90f76e238b
ChirreTTe <ruizkelly28@gmail.com>
2026-09-17 19:59:47 -0500

    Restaurante Chowder

---
+++ proyecto_Chowder/assets/js/layout.js
@@ -0,0 +1,14 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("navbar", "/assets/components/navbar.html");
+    loadComponent("footer", "/assets/components/footer.html");
+});
+
+
